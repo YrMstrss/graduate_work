ad77b12d5f81909e3f769e3a61c7d9c1da26d9d2
ValeryYaku <ya.lera55555@gmail.com>
2023-12-11 20:27:58 +0300

    created model 'User'

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class User(AbstractUser):
+    birthday = models.DateField(verbose_name='дата рождения', **NULLABLE)
+    city = models.CharField(max_length=100, verbose_name='город', **NULLABLE)
+    phone = models.CharField(unique=True, max_length=100, verbose_name='номер телефона')
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    username = models.CharField(max_length=100)
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name}'
+
+    class Meta:
+
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
