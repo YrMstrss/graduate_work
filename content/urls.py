94e48e5f93c14ac91f5d9afa3841e6609818c25b
ValeryYaku <ya.lera55555@gmail.com>
2023-12-12 16:12:19 +0300

    url for list 'Publication'

--- content/urls.py
+++ content/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from content.apps import ContentConfig
-from content.views import PublicationCreateView
+from content.views import PublicationCreateView, PublicationListView
 
 app_name = ContentConfig.name
 
 urlpatterns = [
     path('create/', PublicationCreateView.as_view(), name='create-publication'),
+    path('', PublicationListView.as_view(), name='list-publications'),
 ]
