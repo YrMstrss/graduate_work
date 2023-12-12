13b31a2fc8631e14d04ab020135f5e8f26799de8
ValeryYaku <ya.lera55555@gmail.com>
2023-12-12 15:56:28 +0300

    created url for creation 'Publication'

--- content/urls.py
+++ content/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 from content.apps import ContentConfig
-
+from content.views import PublicationCreateView
 
 app_name = ContentConfig.name
 
 urlpatterns = [
-
+    path('create/', PublicationCreateView.as_view(), name='create-publication'),
 ]
