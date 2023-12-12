17684b189e2dc2e771231b0a138cddb1dfddaac3
ValeryYaku <ya.lera55555@gmail.com>
2023-12-12 15:20:17 +0300

    created model 'Publication'

--- content/models.py
+++ content/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Publication(models.Model):
+    title = models.CharField(max_length=100, verbose_name='заголовок')
+    content = models.TextField(verbose_name='содержание')
+    publication_date = models.DateTimeField(auto_now_add=True, verbose_name='дата публикации')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Публикация'
+        verbose_name_plural = 'Публикации'
