8848fcb1b4eb9e448044f7736fbdacf56d1e0da6
ValeryYaku <ya.lera55555@gmail.com>
2023-12-12 15:55:42 +0300

    added creation controller

--- content/views.py
+++ content/views.py
@@ -1,5 +1,15 @@
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
+
+from content.forms import PublicationForm
+from content.models import Publication
 
 
 class HomePage(TemplateView):
     template_name = 'content/home.html'
+
+
+class PublicationCreateView(CreateView):
+    model = Publication
+    form_class = PublicationForm
+    success_url = reverse_lazy('home')
