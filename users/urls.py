9bbc44e603d44c6bac2bdc4d776ddb678f038e62
ValeryYaku <ya.lera55555@gmail.com>
2023-12-12 15:56:28 +0300

    created url for registration

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
+from django.urls import path
+
 from users.apps import UsersConfig
+from users.views import RegisterView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
 
 ]
