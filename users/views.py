3d1db344d8423721b13803f48fe5af81a3292aee
ValeryYaku <ya.lera55555@gmail.com>
2023-12-13 22:28:31 +0300

    changing User profile

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, UserProfileChangeForm
 from users.models import User
 
 
@@ -10,3 +11,13 @@ class RegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('home')
+
+
+class ProfileView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = UserProfileChangeForm
+    template_name = 'users/profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self, queryset=None):
+        return self.request.user
