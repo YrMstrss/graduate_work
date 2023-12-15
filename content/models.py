a20851b95086099c97399c5403f42d4c503db94d
ValeryYaku <ya.lera55555@gmail.com>
2023-12-15 15:56:10 +0300

    made field 'author' for 'Publication' nullable

--- content/models.py
+++ content/models.py
@@ -2,13 +2,15 @@ from django.db import models
 
 from users.models import User
 
+NULLABLE = {'null': True, 'blank': True}
+
 
 class Publication(models.Model):
     title = models.CharField(max_length=100, verbose_name='заголовок')
     content = models.TextField(verbose_name='содержание')
     publication_date = models.DateTimeField(auto_now_add=True, verbose_name='дата публикации')
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='автор публикации')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='автор публикации', **NULLABLE)
 
     def __str__(self):
         return f'{self.title}'
