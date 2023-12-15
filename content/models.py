fa6c6c439f8fb39d9e14d8020907f236cbc1353a
ValeryYaku <ya.lera55555@gmail.com>
2023-12-15 16:33:29 +0300

    added fields 'publication_type' anf 'views' for 'Publication'

--- content/models.py
+++ content/models.py
@@ -4,6 +4,12 @@ from users.models import User
 
 NULLABLE = {'null': True, 'blank': True}
 
+TYPE_CHOICES = [
+    ('for_all', 'доступна всем пользователям'),
+    ('for_reg', 'доступна зарегистрированным пользователям'),
+    ('for_sub', 'доступна пользователям с оплаченной подпиской'),
+]
+
 
 class Publication(models.Model):
     title = models.CharField(max_length=100, verbose_name='заголовок')
@@ -11,6 +17,10 @@ class Publication(models.Model):
     publication_date = models.DateTimeField(auto_now_add=True, verbose_name='дата публикации')
 
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='автор публикации', **NULLABLE)
+
+    publication_type = models.CharField(max_length=7, choices=TYPE_CHOICES, default='for_all',
+                                        verbose_name='тип публикации')
+    views = models.IntegerField(default=0, verbose_name='просмотры')
 
     def __str__(self):
         return f'{self.title}'
