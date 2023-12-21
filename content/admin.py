6a0aaed525bf216f3481b20afa055431fa295bab
ValeryYaku <ya.lera55555@gmail.com>
2023-12-21 17:09:36 +0300

    added likes to admin page

--- content/admin.py
+++ content/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from content.models import Publication
+from content.models import Publication, Likes
 
 
 @admin.register(Publication)
 class NewsletterAdmin(admin.ModelAdmin):
     list_display = ('pk', 'title', 'content', 'publication_date', )
+
+
+@admin.register(Likes)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'publication', 'is_active')
