74200e110719f8413e795ffc9daabad7dc8ffcb5
ValeryYaku <ya.lera55555@gmail.com>
2024-01-02 17:11:11 +0300

    created PublicationTestCase

--- content/tests.py
+++ content/tests.py
@@ -1,18 +1,45 @@
 from django.test import TestCase, Client
 
+from content.models import Publication
 from users.models import User
+
+
+class PublicationTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user_1 = User.objects.create(first_name='Test', last_name='Testov', phone='89998887766', username='test')
+        user_1.set_password('12345')
+        user_1.save()
+        cls.data = {"title": "test_title", "content": "test_content", 'is_paid': False}
+        cls.post = Publication.objects.create(title='title', content='content', is_paid=True, author=user_1)
+
+    def test_create_post(self):
+        client = Client()
+        client.login(phone="89998887766", password="12345")
+        response = client.post("/publication/create/",
+                               data={"title": "test_title", "content": "test_content", 'is_paid': False})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/publication/2/')
+
+    def test_read_post(self):
+        client = Client()
+        client.login(phone="89998887766", password="12345")
+        response = client.get(f"/publication/{self.post.pk}/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('title', response.content.decode())
+        self.assertIn('content', response.content.decode())
 
 
 class UserTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
-        user_1 = User.objects.create(first_name='Test', last_name='Testov',phone='89998887766', username='test')
-        user_1.set_password('12345')
-        user_1.save()
-        user_2 = User.objects.create(first_name='Test2', last_name='Testov', phone='81112223344', username='test2')
-        user_2.subscriptions.add(user_1)
-        user_2.set_password('54321')
-        user_2.save()
+        cls.user_1 = User.objects.create(first_name='Test', last_name='Testov', phone='89998887766', username='test')
+        cls.user_1.set_password('12345')
+        cls.user_1.save()
+        cls.user_2 = User.objects.create(first_name='Test2', last_name='Testov', phone='81112223344', username='test2')
+        cls.user_2.subscriptions.add(cls.user_1)
+        cls.user_2.set_password('54321')
+        cls.user_2.save()
 
     def test_login(self):
         client = Client()
@@ -33,7 +60,7 @@ class UserTestCase(TestCase):
     def test_other_profile(self):
         client = Client()
         client.login(phone="89998887766", password="12345")
-        response = client.get("/user/profile/2")
+        response = client.get(f"/user/profile/{self.user_2.pk}")
         self.assertEqual(response.status_code, 200)
         self.assertIn('81112223344', response.content.decode())
         self.assertIn('test2', response.content.decode())
