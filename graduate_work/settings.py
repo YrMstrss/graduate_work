6e591bc724e6892b99646b93d2f05e3fd67319a5
ValeryYaku <ya.lera55555@gmail.com>
2024-01-02 14:05:15 +0300

    removed HOST

--- graduate_work/settings.py
+++ graduate_work/settings.py
@@ -87,9 +87,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
+        'USER': os.getenv('POSTGRES_USER'), 
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': 'db'
+        # 'HOST': 'db'
     }
 }
 
